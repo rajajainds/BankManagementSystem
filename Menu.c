8d3287bfcdbe56c72998462ec206a426483298cf
meetrajajain <meetrajajain@gmail.com>
2025-08-04 19:25:57 +0530

    Initial Commit -- Menu Module added

---
+++ Menu.c
@@ -0,0 +1,50 @@
+//Menu module for displaying menu of system to choose operation 
+#include<stdio.h>
+#include<stdlib.h>
+#include "Menu.h"
+#include "Utilities.h"
+
+//function to display menu options and get choosen input 
+int Menu(){
+    int menuChoice;
+    int pass;
+    do{
+    pass=0;//false logic
+    printf("[1]-->Deposit\n");
+    printf("[2]-->Withdraw\n");
+    printf("[3]-->Existing User(Login)\n");
+    printf("[4]-->New User(Regsiter)\n");
+    printf("[0]-->Exit\n");
+    printf("Enter Your Choice : ");
+    scanf("%d",&menuChoice);
+
+    
+
+    switch (menuChoice){
+        case 1:
+        printf("You have choosen Deposit\n");
+        break;
+        case 2:
+        printf("You have choosen Withdraw\n");
+        break;
+        case 3:
+        printf("You have choosen to Login\n");
+        break;
+        case 4:
+        printf("You have choosen to Register\n");
+        break;
+        case 0:
+        printf("You have choosen Exit\n");
+        exit(0);
+        break;
+        default:
+        printf("Inavlid Choice!!! Choose again !!!\n");  
+        pass=1;//true logic
+        ClearInputBuffer();//clearing input buffer so that no ambiguity occur
+        
+    }}while(pass);
+
+ return menuChoice;
+}
+
+
---
+++ Menu.h
@@ -0,0 +1,8 @@
+//Menu.h -->header file for Menu module 
+#ifndef MENU_H
+#define MENU_H
+
+//declaring Menu function
+int Menu();
+
+#endif
