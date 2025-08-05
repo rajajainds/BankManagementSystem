74a59a1b09dae0c617606b973d44b83037347591
meetrajajain <meetrajajain@gmail.com>
2025-08-05 23:58:12 +0530

    Added Manager module Menu

---
+++ Manager.c
@@ -0,0 +1,72 @@
+//Manager module --> to login and approve or decline applications for creation or deletion of accounts
+//including header files
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "Manager.h"
+#include "Menu.h"
+#include "Utilities.h"
+
+//defining functions of Manager Module
+int approveAccountCreation(){
+    char *file="Registrations.txt";
+    FILE *fp=fopen(file,"r");
+    int lines=readLines(file);
+    if (fp!=NULL){
+        printf("No Application Pending !!!\n");
+    }
+    else if (lines>0){
+        printf("Choose which Application to review\n");
+
+    }
+    
+
+    return 0;
+}
+
+void Manager(){
+    //initialising manager credentails for  manager login
+    char managerPass[]="Manager@Bank";
+    char password[20];
+    ClearInputBuffer();
+    printf("Enetr the Password :");
+    fgets(password,sizeof(password),stdin);
+    
+
+    //granting access if password is correct 
+    int managerChoice;
+    if (strcmp(password,managerPass)){
+        printf("You have logged in as Manager\n");
+        printf("Choose from below options :\n");
+        printf("[1]-Approve Account Creation\n");
+        printf("[2]-Approve Account Delation\n");
+        printf("[3]-Update Accounts Details\n");
+        printf("[4]-Update Accounts balance\n");
+        printf("[0]-Go Back\n");
+        printf("Enter Your Choice : ");
+        scanf("%d",&managerChoice);
+
+        switch (managerChoice){
+            case 1:
+            printf("You have Choosen to Approve Account Creation \n");
+            break;
+            case 2:
+            printf("You have Choosen to Approve Account Delation \n");
+            break;
+            case 3:
+            printf("You have Choosen to Update Account Details \n");
+            break;
+            case 4:
+            printf("You have Choosen to Update Account Balance \n");
+            break;
+            case 0:
+            printf("You have Choosen to Go Back \n");
+            Menu();
+            break;
+        }
+    } 
+    else{
+        printf("You have entered Wrong Password\n");
+        Menu();
+    }
+}
---
+++ Manager.h
@@ -0,0 +1,10 @@
+//Manager.h -->header fiel for Manager module
+#ifndef MANAGAER_H
+#define MANAGAER_H
+
+
+//declaring methods of Manager module
+int approveAccountCreation();
+
+void Manager();
+#endif
