29fd32b9d9aa51e99d14ca7277c176ecedfcb9ce
meetrajajain <meetrajajain@gmail.com>
2025-08-05 21:43:47 +0530

    Register added to menu

--- Menu.c
+++ Menu.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include "Menu.h"
 #include "Utilities.h"
+#include "Register.h"
 
 //function to display menu options and get choosen input 
 int Menu(){
@@ -32,6 +33,7 @@ int Menu(){
         break;
         case 4:
         printf("You have choosen to Register\n");
+        Register();
         break;
         case 0:
         printf("You have choosen Exit\n");
